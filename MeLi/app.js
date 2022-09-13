e15f5bb67e9cd613be5a6ff21c7a9d27e13a81ae
sergioescobarh <sergio.escobar.henao@gmail.com>
2022-09-12 20:11:29 -0500

    minimo viable

--- MeLi/app.js
+++ MeLi/app.js
@@ -1,11 +1,14 @@
 const express= require('express');
 const app = express();
 const path= require('path');
+const publicPath = path.resolve(__dirname, '../Public');
+app.use(express.static(publicPath));
+
 app.get('/', function(req, res) {
     let htmlpath=path.join(__dirname,'Vews/home.html');
     res.sendFile(htmlpath);
 });
-app.listen(3000,()=>{console.log('listening on port 3000');});
-let publicPath = path.resolve(__dirname, '../Public');
-app.use(express.static(publicPath));
+app.listen(process.env.PORT||3000,()=>{console.log('listening on port 3000');});
+
+
 //agregar register!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
