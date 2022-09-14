0f1ca6224be96a86df4d13ea44d0d64cdbf4f949
sergioescobarh <sergio.escobar.henao@gmail.com>
2022-09-14 15:01:32 -0500

    MeLi para enviar

--- MeLi/app.js
+++ MeLi/app.js
@@ -1,14 +1,40 @@
 const express= require('express');
 const app = express();
 const path= require('path');
+//Middleware
 const publicPath = path.resolve(__dirname, '../Public');
 app.use(express.static(publicPath));
-
+app.listen(process.env.PORT||3000,function(){console.log('listening on port 3000');});
+//rUTAS:
 app.get('/', function(req, res) {
     let htmlpath=path.join(__dirname,'Vews/home.html');
     res.sendFile(htmlpath);
 });
-app.listen(process.env.PORT||3000,()=>{console.log('listening on port 3000');});
+app.get('/', function(req, res) {
+    let htmlregisterpath=path.join(__dirname,'Vews/register.html');
+    res.sendFile(htmlregisterpath);
+});
+app.get('/', function(req, res) {
+    let htmlloginpath=path.join(__dirname,'Vews/login.html');
+    res.sendFile(htmlloginpath);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //agregar register!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
